b15dbde159dd4b936ded0652bcd9bd100092f18f
SantyR-Glitch <stgrosales@gmail.com>
2026-08-25 18:16:14 -0600

    Add files via upload

---
+++ python.py
@@ -0,0 +1,7 @@
+Calificacion1 = int(input("Introduce tu primera calificación: "))
+Calificacion2 = int(input("Introduce tu segunda calificación: "))
+Calificacion3 = int(input("Introduce tu tercera calificación: "))
+
+Promedio = (Calificacion1 + Calificacion2 + Calificacion3) / 3
+
+print("La calificación promedio es:", Promedio)
